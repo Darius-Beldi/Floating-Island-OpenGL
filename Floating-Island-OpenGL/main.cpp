0c9c9a4cc086660de3309824d5ad703b9af29d1e
Darius <beldi_darius@yahoo.com>
2026-01-10 20:33:29 +0200

    Try Again

--- Floating-Island-OpenGL/main.cpp
+++ Floating-Island-OpenGL/main.cpp
@@ -1,122 +1,265 @@
-// Codul sursa este adaptat dupa OpenGLBook.com
+﻿//
+// ================================================
+// | Grafica pe calculator                        |
+// ================================================
+// | Laboratorul X - 10_01_modele3D.cpp |
+// =============================================
+// 
+// Program care deseneaza un model 3D importat  
 
+// Biblioteci
 #include <windows.h>  // biblioteci care urmeaza sa fie incluse
-#include <stdlib.h> // necesare pentru citirea shader-elor
 #include <stdio.h>
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <cstdlib> 
+#include <vector>
+#include <math.h>
+#include <iostream>
 #include <GL/glew.h> // glew apare inainte de freeglut
 #include <GL/freeglut.h> // nu trebuie uitat freeglut.h
+
 #include "loadShaders.h"
 
+#include "glm/glm.hpp"  
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "objloader.hpp"  
 
-//////////////////////////////////////
 
+//  Identificatorii obiectelor de tip OpenGL;
 GLuint
 VaoId,
 VboId,
-ColorBufferId,
-ProgramId;
+ProgramId,
+nrVertLocation,
+myMatrixLocation,
+viewPosLocation,
+viewLocation,
+projLocation;
+
+// Valoarea lui pi
+float PI = 3.141592;
+
+// Variabila pentru numarul de varfuri
+int nrVertices;
+
+// Vectori pentru varfuri, coordonate de texturare, normale
+std::vector<glm::vec3> vertices;
+std::vector<glm::vec2> uvs;
+std::vector<glm::vec3> normals;
+std::vector<glm::vec3> colors;
+
+// Matrice utilizate
+glm::mat4 myMatrix;
+glm::mat4 view;
+glm::mat4 projection;
+
+//	Elemente pentru matricea de vizualizare;
+float refX = 0.0f, refY = 0.0f, refZ = 0.0f,
+obsX, obsY, obsZ,
+vX = 0.0f, vY = 0.0f, vZ = 1.0f;
+//	Elemente pentru deplasarea pe sfera;
+float alpha = 0.0f, beta = 0.0f, dist = 6.0f,
+incrAlpha1 = 0.01, incrAlpha2 = 0.01;
+//	Elemente pentru matricea de proiectie;
+float width = 800, height = 600, dNear = 4.f, fov = 60.f * PI / 180;
 
 
+void processNormalKeys(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case '+':
+		dist -= 0.25;	//	apasarea tastelor `-` si `+` schimba pozitia observatorului (se departeaza / aproprie);
+		break;
+	case '-':
+		dist += 0.25;
+		break;
+	}
+	if (key == 27)
+		exit(0);
+}
+
+void processSpecialKeys(int key, int xx, int yy)
+{
+	switch (key)				//	Procesarea tastelor 'LEFT', 'RIGHT', 'UP', 'DOWN';
+	{							//	duce la deplasarea observatorului pe suprafata sferica in jurul cubului;
+	case GLUT_KEY_LEFT:
+		beta -= 0.01;
+		break;
+	case GLUT_KEY_RIGHT:
+		beta += 0.01;
+		break;
+	case GLUT_KEY_UP:
+		alpha += incrAlpha1;
+		if (abs(alpha - PI / 2) < 0.05)
+		{
+			incrAlpha1 = 0.f;
+		}
+		else
+		{
+			incrAlpha1 = 0.01f;
+		}
+		break;
+	case GLUT_KEY_DOWN:
+		alpha -= incrAlpha2;
+		if (abs(alpha + PI / 2) < 0.05)
+		{
+			incrAlpha2 = 0.f;
+		}
+		else
+		{
+			incrAlpha2 = 0.01f;
+		}
+		break;
+	}
+}
+
+// Se initializeaza un vertex Buffer Object(VBO) pentru transferul datelor spre memoria placii grafice(spre shadere);
+// In acesta se stocheaza date despre varfuri;
 void CreateVBO(void)
 {
-	// varfurile 
-	GLfloat Vertices[] = {
-		0.5f,  0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.0f, 1.0f,
-		-0.5f,  0.5f, 0.0f, 1.0f,
-		0.5f,  0.5f, 0.0f, 1.0f
-	};
 
-	// culorile, ca atribute ale varfurilor
-	GLfloat Colors[] = {
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	  1.0f, 0.5f, 0.2f, 1.0f,
-	};
-
-	// se creeaza un buffer nou
-	glGenBuffers(1, &VboId);
-	// este setat ca buffer curent
-	glBindBuffer(GL_ARRAY_BUFFER, VboId);
-	// varfurile sunt "copiate" in bufferul curent
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-	// se creeaza / se leaga un VAO (Vertex Array Object) - util cand se utilizeaza mai multe VBO
+	// Generare VAO;
 	glGenVertexArrays(1, &VaoId);
 	glBindVertexArray(VaoId);
-	// se activeaza lucrul cu atribute; atributul 0 = pozitie
-	glEnableVertexAttribArray(0);
-	// 
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
-	// un nou buffer, pentru culoare
-	glGenBuffers(1, &ColorBufferId);
-	glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
-	// atributul 1 =  culoare
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	// Generare VBO - varfurile si normalele sunt memorate in sub-buffere;
+	glGenBuffers(1, &VboId);
+	glBindBuffer(GL_ARRAY_BUFFER, VboId);
+
+	// Alocă spațiu pentru poziții, normale și culori
+	glBufferData(GL_ARRAY_BUFFER,
+		vertices.size() * sizeof(glm::vec3) +
+		normals.size() * sizeof(glm::vec3) +
+		colors.size() * sizeof(glm::vec3),
+		NULL, GL_STATIC_DRAW);
+
+	// Copiază datele
+	glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(glm::vec3), &vertices[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), normals.size() * sizeof(glm::vec3), &normals[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3) + normals.size() * sizeof(glm::vec3), colors.size() * sizeof(glm::vec3), &colors[0]);
+
+	// Atributele; 
+	glEnableVertexAttribArray(0); // atributul 0 = pozitie
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glEnableVertexAttribArray(1); // atributul 1 = normale
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)(vertices.size() * sizeof(glm::vec3)));
+	glEnableVertexAttribArray(2); // atributul 2 = culoare
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)(vertices.size() * sizeof(glm::vec3) + normals.size() * sizeof(glm::vec3)));
+
 }
+
+//  Eliminarea obiectelor de tip VBO dupa rulare;
 void DestroyVBO(void)
 {
-	glDisableVertexAttribArray(1);
 	glDisableVertexAttribArray(0);
-
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glDeleteBuffers(1, &ColorBufferId);
-	glDeleteBuffers(1, &VboId);
-
 	glBindVertexArray(0);
 	glDeleteVertexArrays(1, &VaoId);
 }
 
+//  Crearea si compilarea obiectelor de tip shader;
 void CreateShaders(void)
 {
-	ProgramId = LoadShaders("example.vert", "example.frag");
+	ProgramId = LoadShaders("10_01_Shader.vert", "10_01_Shader.frag");
 	glUseProgram(ProgramId);
 }
+
+// Elimina obiectele de tip shader dupa rulare;
 void DestroyShaders(void)
 {
 	glDeleteProgram(ProgramId);
 }
 
-void Initialize(void)
-{
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // culoarea de fond a ecranului
-	CreateVBO();
-	CreateShaders();
-}
-void RenderFunction(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT);       
-
-	// Functiile de desenare
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDrawArrays(GL_TRIANGLES, 3, 3);
-
-	glFlush();
-}
+//  Functia de eliberare a resurselor alocate de program;
 void Cleanup(void)
 {
 	DestroyShaders();
 	DestroyVBO();
+};
+
+//  Setarea parametrilor necesari pentru fereastra de vizualizare;
+void Initialize(void)
+{
+	glClearColor(0.95f, 0.82f, 0.4f, 1.0f); // culoarea de fond a ecranului
+
+	// Incarcarea modelului 3D in format OBJ
+	bool model = loadOBJ("Assets/Scena.obj", vertices, uvs, normals, colors);
+	nrVertices = vertices.size();
+
+	// Crearea VBO / shadere-lor
+	CreateVBO();
+	CreateShaders();
+
+	// Locatii ptr shader
+	nrVertLocation = glGetUniformLocation(ProgramId, "nrVertices");
+	myMatrixLocation = glGetUniformLocation(ProgramId, "myMatrix");
+	viewPosLocation = glGetUniformLocation(ProgramId, "viewPos");
+	viewLocation = glGetUniformLocation(ProgramId, "view");
+	projLocation = glGetUniformLocation(ProgramId, "projection");
+
+	// Variabile ce pot fi transmise catre shader
+	glUniform1i(ProgramId, nrVertices);
 }
 
+//	Functia de desenare a graficii pe ecran;
+void RenderFunction(void)
+{
+	// Initializare ecran + test de adancime;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+	// Matricea de modelare 
+	myMatrix = glm::rotate(glm::mat4(1.0f), PI / 2, glm::vec3(0.0, 1.0, 0.0))
+		* glm::rotate(glm::mat4(1.0f), PI / 2, glm::vec3(0.0, 0.0, 1.0));
+	glUniformMatrix4fv(myMatrixLocation, 1, GL_FALSE, &myMatrix[0][0]);
+
+	//	Vizualizare;
+	//	Pozitia observatorului - se deplaseaza pe sfera;
+	obsX = refX + dist * cos(alpha) * cos(beta);
+	obsY = refY + dist * cos(alpha) * sin(beta);
+	obsZ = refZ + dist * sin(alpha);
+	//	Vectori pentru matricea de vizualizare;
+	glm::vec3 obs = glm::vec3(obsX, obsY, obsZ);		//	Pozitia observatorului;	
+	glm::vec3 pctRef = glm::vec3(refX, refY, refZ); 	//	Pozitia punctului de referinta;
+	glm::vec3 vert = glm::vec3(vX, vY, vZ);			//	Verticala din planul de vizualizare; 
+	// Pozitia observatorului, transmitere catre shader
+	glUniform3f(viewPosLocation, obsX, obsY, obsZ);
+	// Matricea de vizualizare, transmitere catre shader
+	view = glm::lookAt(obs, pctRef, vert);
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
+
+	//	Proiectie;
+	projection = glm::infinitePerspective(GLfloat(fov), GLfloat(width) / GLfloat(height), dNear);
+	glUniformMatrix4fv(projLocation, 1, GL_FALSE, &projection[0][0]);
+
+	// "Legarea"VAO, desenare;
+	glBindVertexArray(VaoId);
+	glEnableVertexAttribArray(0); // atributul 0 = pozitie
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+	glutSwapBuffers();
+	glFlush();
+}
+
+//	Punctul de intrare in program, se ruleaza rutina OpenGL;
 int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(100, 100); // pozitia initiala a ferestrei
-	glutInitWindowSize(600, 600); //dimensiunile ferestrei
-	glutCreateWindow("Grafica pe calculator - primul exemplu"); // titlul ferestrei
-	glewInit(); // nu uitati de initializare glew; trebuie initializat inainte de a a initializa desenarea
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1200, 900);
+	glutCreateWindow("Utilizarea unui model predefinit in format OBJ");
+	glewInit();
 	Initialize();
+	glutIdleFunc(RenderFunction);
 	glutDisplayFunc(RenderFunction);
+	glutKeyboardFunc(processNormalKeys);
+	glutSpecialFunc(processSpecialKeys);
 	glutCloseFunc(Cleanup);
 	glutMainLoop();
 }
---
+++ Floating-Island-OpenGL/objloader.cpp
@@ -0,0 +1,157 @@
+﻿// Adaptat dupa http://www.opengl-tutorial.org/ 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <vector>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include "glm/glm.hpp"
+
+#include "objloader.hpp"
+
+bool loadMTL(const std::string& path, std::map<std::string, Material>& materials) {
+    FILE* file = fopen(path.c_str(), "r");
+    if (!file) return false;
+
+    char lineHeader[128];
+    Material mat;
+    std::string currentMat;
+    while (fscanf(file, "%s", lineHeader) != EOF) {
+        if (strcmp(lineHeader, "newmtl") == 0) {
+            if (!currentMat.empty()) {
+                materials[currentMat] = mat;
+            }
+            char name[128];
+            fscanf(file, "%s\n", name);
+            currentMat = name;
+            mat = Material();
+            mat.name = currentMat;
+        }
+        else if (strcmp(lineHeader, "Kd") == 0) {
+            fscanf(file, "%f %f %f\n", &mat.diffuseColor.r, &mat.diffuseColor.g, &mat.diffuseColor.b);
+        }
+        else {
+            char buffer[1024];
+            fgets(buffer, 1024, file);
+        }
+    }
+    if (!currentMat.empty()) {
+        materials[currentMat] = mat;
+    }
+    fclose(file);
+    return true;
+}
+
+bool loadOBJ(
+	const char* path,
+	std::vector<glm::vec3>& out_vertices,
+	std::vector<glm::vec2>& out_uvs,
+	std::vector<glm::vec3>& out_normals,
+	std::vector<glm::vec3>& out_colors // nou!
+)
+{
+    printf("Loading OBJ file %s...\n", path);
+
+    std::vector<unsigned int> vertexIndices, uvIndices, normalIndices, materialIndices;
+    std::vector<glm::vec3> temp_vertices;
+    std::vector<glm::vec2> temp_uvs;
+    std::vector<glm::vec3> temp_normals;
+    std::vector<std::string> faceMaterials;
+
+    std::map<std::string, Material> materials;
+    std::string currentMaterial = "";
+    std::string mtlFile = "";
+
+    // Determină directorul fișierului OBJ
+    std::string objPath(path);
+    std::string directory = objPath.substr(0, objPath.find_last_of("/\\") + 1);
+
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        printf("Impossible to open the file !\n");
+        return false;
+    }
+
+    char lineHeader[128];
+    while (fscanf(file, "%s", lineHeader) != EOF) {
+        if (strcmp(lineHeader, "v") == 0) {
+            glm::vec3 vertex;
+            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+            temp_vertices.push_back(vertex);
+        }
+        else if (strcmp(lineHeader, "vt") == 0) {
+            glm::vec2 uv;
+            fscanf(file, "%f %f\n", &uv.x, &uv.y);
+            uv.y = -uv.y;
+            temp_uvs.push_back(uv);
+        }
+        else if (strcmp(lineHeader, "vn") == 0) {
+            glm::vec3 normal;
+            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+            temp_normals.push_back(normal);
+        }
+        else if (strcmp(lineHeader, "f") == 0) {
+            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
+                &vertexIndex[0], &uvIndex[0], &normalIndex[0],
+                &vertexIndex[1], &uvIndex[1], &normalIndex[1],
+                &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+            if (matches != 9) {
+                printf("File can't be read by our simple parser :-( Try exporting with other options\n");
+                fclose(file);
+                return false;
+            }
+            for (int i = 0; i < 3; ++i) {
+                vertexIndices.push_back(vertexIndex[i]);
+                uvIndices.push_back(uvIndex[i]);
+                normalIndices.push_back(normalIndex[i]);
+                faceMaterials.push_back(currentMaterial);
+            }
+        }
+        else if (strcmp(lineHeader, "mtllib") == 0) {
+            char mtlName[128];
+            fscanf(file, "%s\n", mtlName);
+            mtlFile = directory + mtlName;
+            loadMTL(mtlFile, materials);
+        }
+        else if (strcmp(lineHeader, "usemtl") == 0) {
+            char matName[128];
+            fscanf(file, "%s\n", matName);
+            currentMaterial = matName;
+        }
+        else {
+            char buffer[1024];
+            fgets(buffer, 1024, file);
+        }
+    }
+    fclose(file);
+
+    // Asamblează datele finale, inclusiv culoarea
+    for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+        unsigned int vertexIndex = vertexIndices[i];
+        unsigned int uvIndex = uvIndices[i];
+        unsigned int normalIndex = normalIndices[i];
+
+        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+        glm::vec2 uv = temp_uvs[uvIndex - 1];
+        glm::vec3 normal = temp_normals[normalIndex - 1];
+
+        out_vertices.push_back(vertex);
+        out_uvs.push_back(uv);
+        out_normals.push_back(normal);
+
+        // Adaugă culoarea materialului curent
+        glm::vec3 color(0.8f, 0.8f, 0.8f);
+        if (!faceMaterials[i].empty() && materials.count(faceMaterials[i])) {
+            color = materials[faceMaterials[i]].diffuseColor;
+        }
+        out_colors.push_back(color);
+    }
+    return true;
+}
+
+
+
